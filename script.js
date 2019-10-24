7ed459914fcbdf407eaf235d89a0fea70e87ac00
phpatel43 <phpatel43@gmail.com>
2019-10-24 19:49:09 -0400

    character

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ var specialChar = ['!', '#', '$', '%', '&', '(', ')', '*', '+', '-', '"', '.', '
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 var nums = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-var passArr =[]
+var passArr = []
 var count;
 
 document.getElementById("generate").addEventListener("click", function(){
     length()
 })
 
-function length(){
+function length() {
     count = prompt("How long would you like your password?")
     console.log(count)
     if (count > 128) {
@@ -24,6 +24,29 @@ function length(){
     }
 }
 
+function Character() {
+    passArr=[]
+    if (confirm("Would you like special characters?")) {
+        passArr.push(specialChar)
+    }
+    if (confirm("Would you like uppercase characters?")) {
+        passArr.push(upperCase)
+    }
+    if (confirm("Would you like lowercase characters?")) {
+        passArr.push(lowerCase)
+    }
+    if (confirm("Would you like numbers?")) {
+        passArr.push(nums)
+    }
+    if (passArr.length < 2) {
+        alert("You must select 2 character types.")
+        Character()
+    }
+    else {
+        generate(passArr, count)
+    }
+    console.log(passArr)
+}
 
 
 document.getElementById("text").value="test"
